707e7d9c3dbd057c410e1bf7edb10dd7287e89d3
LILITHl <Saosao007888@gmail.com>
2022-04-02 20:22:33 +0800

    car list and index

---
+++ list.py
@@ -0,0 +1,11 @@
+# list 清單
+
+a = ['Toyota', 'Honda']#空清單
+print(a)
+print(a[0])
+print(a[1])
+a.append('Audi')
+print(a)
+print(len(a))
+print('Audi' in a) #奧迪有沒有在a清單裡面#是非題 True,False 
+print('Benz'in a) #in 後面接清單
